5317ce45fcb9033951acdb06d56161246067bf2e
Nathaniel Morck Beaver <nathanielmbeaver@gmail.com>
2019-07-02 10:47:42 -0500

    Call it quit instead.

--- visit_paths.py
+++ visit_paths.py
@@ -70,11 +70,11 @@ if __name__ == '__main__':
             continue
         if i != 0:
             try :
-                response = input("#{}. Enter 'n' to stop or just enter to continue: ".format(n_visits + 1))
+                response = input("#{}. Enter 'q' to quit or just enter to continue: ".format(n_visits + 1))
             except EOFError:
                 sys.stdout.write('\n')
                 break
-            if response in ["n", "no"]:
+            if response in ["q", "quit", "exit"]:
                 break
         logging.info("spawning '{}' in '{}'".format(shell_bin, visit_dir))
         run_args = [shell_bin, "-i"]
